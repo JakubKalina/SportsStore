a67f6f43ad5b09f43b5c132a7d8699181a2c72f5
Jakub Kalina <jakub.kalina@codeteam.pl>
2019-11-06 13:42:05 +0100

    Added controller, list action result and it's view and Razor pages settings

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
 
 namespace SportsStore.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private IProductRepository productRepository;
+
+        public ProductController(IProductRepository repo)
         {
-            return View();
+            productRepository = repo;
         }
+
+        public ViewResult List() => View(productRepository.Products);
     }
 }
